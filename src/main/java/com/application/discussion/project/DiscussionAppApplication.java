03064fa7371e4681fb69d2ea137b815e2647b7cb
azla mahryu <sgommccisbad72145@gmail.com>
2024-12-18 22:23:39 +0900

    Pj first commit

---
+++ src/main/java/com/application/discussion/project/DiscussionAppApplication.java
@@ -0,0 +1,13 @@
+package com.application.discussion.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/discussion/project/DiscussionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.discussion.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
