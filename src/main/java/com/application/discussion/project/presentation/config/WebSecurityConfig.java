f705c3f949db4ff74e5411a8a61ba72948547d4c
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-07-15 00:47:29 +0900

    add presentation config web security config

--- src/main/java/com/application/discussion/project/presentation/config/WebSecurityConfig.java
+++ src/main/java/com/application/discussion/project/presentation/config/WebSecurityConfig.java
@@ -1,25 +1,75 @@
 package com.application.discussion.project.presentation.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.application.discussion.project.application.services.security.JWTAuthUserDetailsService;
+import com.application.discussion.project.presentation.security.JWTAuthEntryPoint;
+import com.application.discussion.project.presentation.security.JWTAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    
+    @Autowired
+    private JWTAuthEntryPoint jwtAuthEntryPoint;
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private JWTAuthUserDetailsService jwtAuthUserDetailsService;
+
+
+    // @Bean
+    // public DaoAuthenticationProvider daoAuthenticationProvider(){
+    //     DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        
+    //     daoAuthenticationProvider.setUserDetailsService(jwtAuthUserDetailsService);
+    //     daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+    //     return daoAuthenticationProvider;
+    // }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeHttpRequests(
+        httpSecurity.csrf(csrf -> csrf.disable())
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(
             (authorize) -> authorize.requestMatchers(
                 "/v3/api-docs/**",
                 "/swagger-ui/**",
                 "/swagger-ui/index.html",
                 "/swagger-ui.html"
             ).permitAll()
-        );
+            .requestMatchers("/api/auth/v1/login").permitAll()
+            .anyRequest().authenticated()
+            );
+
+        // httpSecurity.authenticationProvider(daoAuthenticationProvider());
+        
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
