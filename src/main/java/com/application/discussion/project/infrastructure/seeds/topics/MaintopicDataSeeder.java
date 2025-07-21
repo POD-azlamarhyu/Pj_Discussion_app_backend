9c2f2bac20c47d1ea8027b48bca5f7356413d3bc
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-07-21 11:35:00 +0900

    feat (): #25 maintopic-list-api add file to seeder maintopic data

---
+++ src/main/java/com/application/discussion/project/infrastructure/seeds/topics/MaintopicDataSeeder.java
@@ -0,0 +1,32 @@
+package com.application.discussion.project.infrastructure.seeds.topics;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.application.discussion.project.infrastructure.models.topics.Maintopics;
+import com.application.discussion.project.infrastructure.repositories.topics.JpaMaintopicsRepository;
+
+@Component
+@Profile("dev")
+public class MaintopicDataSeeder {
+
+    private final List<Maintopics> maintopicsSeedList = List.of(
+        new Maintopics(null, "日本の政治体制の危うさについて", "日本の政治体制について議論する場所", null, null, false, false),
+        new Maintopics(null, "古典・漢文を高校で学ぶ意義とはなにか", "社会にでれば全く役に立たない古典・漢文を日本の高校で勉強することの意義と問題を議論する場", null, null, false, false),
+        new Maintopics(null, "日本政府の増税信仰について", "日本政府，とりわけ自民党が掲げる増税をなぜこの不景気のときに進めるのか，その対策を議論する場", null, null, false, false),
+        new Maintopics(null, "Support", "Get help and support from the community", null, null, false, false),
+        new Maintopics(null, "某隣の国の静かな侵略に関する", "Discuss anything that doesn't fit in other categories", null, null, false, false)
+    );
+    
+    @Autowired
+    private JpaMaintopicsRepository jpaMaintopicsRepository;
+
+    public void seed(){
+        maintopicsSeedList.stream()
+            .filter(maintopic -> !jpaMaintopicsRepository.existsByTitle(maintopic.getTitle()))
+            .forEach(maintopic -> jpaMaintopicsRepository.save(maintopic));
+    }
+}
