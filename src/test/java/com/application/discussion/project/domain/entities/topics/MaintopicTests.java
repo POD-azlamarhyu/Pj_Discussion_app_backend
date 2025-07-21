b84da85c938cab181335ed116486769fe1ac1c2b
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-07-22 03:21:32 +0900

    text (): #25 add file to unit test domain enntity maintopic tests

---
+++ src/test/java/com/application/discussion/project/domain/entities/topics/MaintopicTests.java
@@ -0,0 +1,54 @@
+package com.application.discussion.project.domain.entities.topics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class MaintopicTests {
+    private Maintopic maintopic;
+
+    @BeforeEach
+    void setUp(){
+        maintopic = Maintopic.of(
+            1L, 
+            "Sample Title", 
+            "Sample Description", 
+            LocalDateTime.of(2026, 7, 10, 12, 0, 0), 
+            LocalDateTime.of(2026, 7, 10, 12, 0, 0),
+            false, 
+            false
+        );
+    }
+
+    void testGetMaintopicId() throws Exception {
+        assertEquals(1L, maintopic.getMaintopicId());
+    }
+
+    void testGetTitle() throws Exception {
+        assertEquals("Sample Title", maintopic.getTitle());
+    }
+    void testGetDescription() throws Exception {
+        assertEquals("Sample Description", maintopic.getDescription());
+    }
+
+    void testGetCreatedAt() throws Exception {
+        assertEquals(LocalDateTime.of(2026, 7, 10, 12, 0, 0), maintopic.getCreatedAt());
+    }
+
+    void testGetUpdatedAt() throws Exception {
+        assertEquals(LocalDateTime.of(2026, 7, 10, 12, 0, 0), maintopic.getUpdatedAt());
+    }
+
+    void testIsDeleted() throws Exception {
+        assertFalse(maintopic.getIsDeleted());
+    }
+
+    void testIsClosed()  throws Exception {
+        assertFalse(maintopic.getIsClosed());
+    }
+
+
+}
