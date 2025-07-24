ddf470d6ed65e8ca7373ac94a6e14fc5de4edf7f
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-07-25 00:46:32 +0900

    update: () #25 edit maintopliclistserviceimpl for add logging info

--- src/main/java/com/application/discussion/project/application/services/topics/MaintopicsListServiceImpl.java
+++ src/main/java/com/application/discussion/project/application/services/topics/MaintopicsListServiceImpl.java
@@ -3,6 +3,8 @@ package com.application.discussion.project.application.services.topics;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.application.discussion.project.application.dtos.topics.MaintopicListResponse;
@@ -13,12 +15,15 @@ public class MaintopicsListServiceImpl implements MaintopicsListService {
 
     private final MaintopicRepository maintopicRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(MaintopicsListServiceImpl.class);
+
     public MaintopicsListServiceImpl(MaintopicRepository maintopicRepository) {
         this.maintopicRepository = maintopicRepository;
     }
 
     @Override
     public List<MaintopicListResponse> service() {
+        logger.info("Fetching list of maintopics");
         List<MaintopicListResponse> maintopicList = maintopicRepository.findMaintopicList().stream()
                 .map(maintopic -> new MaintopicListResponse(
                         maintopic.getMaintopicId(),
