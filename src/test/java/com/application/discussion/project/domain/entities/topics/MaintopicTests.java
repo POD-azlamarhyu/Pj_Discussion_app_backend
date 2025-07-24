4aac1259bafb7b7c1f4802b58090c756e66f5771
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-07-25 00:51:50 +0900

    update :() #25 edit maintopic tests for add @Test Annotations

--- src/test/java/com/application/discussion/project/domain/entities/topics/MaintopicTests.java
+++ src/test/java/com/application/discussion/project/domain/entities/topics/MaintopicTests.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class MaintopicTests {
     private Maintopic maintopic;
@@ -23,29 +24,37 @@ public class MaintopicTests {
         );
     }
 
+    @Test
     void testGetMaintopicId() throws Exception {
         assertEquals(1L, maintopic.getMaintopicId());
     }
 
+    @Test
     void testGetTitle() throws Exception {
         assertEquals("Sample Title", maintopic.getTitle());
     }
+
+    @Test
     void testGetDescription() throws Exception {
         assertEquals("Sample Description", maintopic.getDescription());
     }
 
+    @Test
     void testGetCreatedAt() throws Exception {
         assertEquals(LocalDateTime.of(2026, 7, 10, 12, 0, 0), maintopic.getCreatedAt());
     }
 
+    @Test
     void testGetUpdatedAt() throws Exception {
         assertEquals(LocalDateTime.of(2026, 7, 10, 12, 0, 0), maintopic.getUpdatedAt());
     }
 
+    @Test
     void testIsDeleted() throws Exception {
         assertFalse(maintopic.getIsDeleted());
     }
 
+    @Test
     void testIsClosed()  throws Exception {
         assertFalse(maintopic.getIsClosed());
     }
