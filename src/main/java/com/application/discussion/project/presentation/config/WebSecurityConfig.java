98727ebb958edb1d7d4607c6fb59b2dcada7ff4c
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-07-27 19:13:40 +0900

    update: (me ) #25 Maintopic List API edit file: - add http security disabled csrf - remove unnecessary imports and add necessary imports - add permittions for Swagger UI and Maintopics

--- src/main/java/com/application/discussion/project/presentation/config/WebSecurityConfig.java
+++ src/main/java/com/application/discussion/project/presentation/config/WebSecurityConfig.java
@@ -2,24 +2,27 @@ package com.application.discussion.project.presentation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf(csrf -> csrf.disable());
         httpSecurity.authorizeHttpRequests(
             (authorize) -> authorize.requestMatchers(
                 "/v3/api-docs/**",
                 "/swagger-ui/**",
                 "/swagger-ui/index.html",
-                "/swagger-ui.html"
+                "/swagger-ui.html",
+                "/maintopics/*"
             ).permitAll()
-            .anyRequest().permitAll()
         );
 
         return httpSecurity.build();
