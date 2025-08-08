3b816564aec28e185ed53fe3ca27e1cd5f1efcd7
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-08-09 03:55:36 +0900

    feat: (me) #23 create maintopic api add file, Maintopic Create Response DTO.

---
+++ src/main/java/com/application/discussion/project/application/dtos/topics/MaintopicCreateResponse.java
@@ -0,0 +1,52 @@
+package com.application.discussion.project.application.dtos.topics;
+
+public class MaintopicCreateResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String createdAt;
+
+    public MaintopicCreateResponse(
+        Long id, 
+        String title,
+        String description, 
+        String createdAt
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
