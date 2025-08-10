9a22f2ed553396bdcfe43fcd8f0633d7753b0022
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-08-10 18:28:51 +0900

    feat: (me) #23 add file, Implemented test code for the Maintopic Title value object.

---
+++ src/test/java/com/application/discussion/project/domain/valueobjects/topics/TitleTests.java
@@ -0,0 +1,55 @@
+package com.application.discussion.project.domain.valueobjects.topics;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.application.discussion.project.domain.exceptions.BadRequestException;
+
+public class TitleTests {
+    private Title title;
+    private final String testValue = "HogeFuga";
+
+    @BeforeEach
+    void setUp(){
+        title = Title.of(testValue);
+    }
+
+    @Test
+    void testGetValue(){
+        assertEquals(testValue, title.getValue());
+    }
+
+    @Test
+    void testCreateInstance(){
+        final Title testHoge = Title.of(testValue);
+        assertNotNull(testHoge);
+        assertInstanceOf(Title.class, testValue);
+    }
+
+    @Test
+    void testCreateInstanceBorder(){
+        final String value = "テスト"; 
+        final Title testFuga = Title.of(value);
+        assertNotNull(testFuga);
+        assertInstanceOf(Title.class, testFuga);
+    }
+
+    @Test
+    void testCreateTopicMinLengthFailure(){
+        final String value = "AI";
+        assertThrows(BadRequestException.class,() -> {Title.of(value);});
+    }
+
+    @Test
+    void testCreateTopicMaxLengthFailure(){
+        String value = "F".repeat(101);
+        assertThrows(BadRequestException.class,() -> {Title.of(value);});
+    }
+
+    @Test
+    void testCreateTopicNull(){
+        final String value = null;
+        assertThrows(BadRequestException.class, () -> {Title.of(value);});
+    }
+}
