cf8849b5f30b72286f83d52ce919cf995fe660c3
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-08-10 22:31:00 +0900

    feat: (me) #23 update file, Implemented a no-argument constructor.

--- src/main/java/com/application/discussion/project/application/dtos/topics/MaintopicCreateRequest.java
+++ src/main/java/com/application/discussion/project/application/dtos/topics/MaintopicCreateRequest.java
@@ -1,33 +1,32 @@
 package com.application.discussion.project.application.dtos.topics;
 
-import lombok.Builder;
 
-@Builder
+
+
 public class MaintopicCreateRequest {
     private String title;
     private String description;
 
-    public MaintopicCreateRequest(
-        String title,
-        String description
-    ){
+    public MaintopicCreateRequest() {}
+    
+    public MaintopicCreateRequest(String title, String description) {
         this.title = title;
         this.description = description;
     }
-
-    public String getTitle() {
-        return title;
+    
+    public String getTitle() { 
+        return title; 
     }
-
-    public void setTitle(String title) {
-        this.title = title;
+    
+    public void setTitle(String title) { 
+        this.title = title; 
     }
-
-    public String getDescription() {
-        return description;
+    
+    public String getDescription() { 
+        return description; 
     }
-
-    public void setDescription(String description) {
-        this.description = description;
+    
+    public void setDescription(String description) { 
+        this.description = description; 
     }
 }
