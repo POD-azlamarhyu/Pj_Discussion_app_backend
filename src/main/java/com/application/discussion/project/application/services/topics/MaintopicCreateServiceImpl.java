ce98b37ba7068798e8a4deb52e41264d203ea2e3
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-08-10 17:47:05 +0900

    feat: (me) #23 update file, Added exception handling and logger to the maintopic registration feature.

--- src/main/java/com/application/discussion/project/application/services/topics/MaintopicCreateServiceImpl.java
+++ src/main/java/com/application/discussion/project/application/services/topics/MaintopicCreateServiceImpl.java
@@ -1,5 +1,7 @@
 package com.application.discussion.project.application.services.topics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.application.discussion.project.application.dtos.topics.MaintopicCreateRequest;
@@ -8,26 +10,36 @@ import com.application.discussion.project.domain.entities.topics.Maintopic;
 import com.application.discussion.project.domain.repositories.MaintopicRepository;
 import com.application.discussion.project.domain.valueobjects.topics.Description;
 import com.application.discussion.project.domain.valueobjects.topics.Title;
+import com.application.discussion.project.application.dtos.exceptions.InternalServerErrorException;
 
 public class MaintopicCreateServiceImpl implements MaintopicCreateService {
     
     @Autowired
     private MaintopicRepository maintopicRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(MaintopicCreateServiceImpl.class);
+
     @Override
     public MaintopicCreateResponse service(final MaintopicCreateRequest maintopicCreateRequest){
         // Validate the request
-        final Title title = Title.of(maintopicCreateRequest.getTitle());
-        final Description description = Description.of(maintopicCreateRequest.getDescription());
-        final Maintopic maintopic = Maintopic.create(title, description);
+        try{
+            logger.info("Creating a new maintopic with title: {}", maintopicCreateRequest.getTitle());
+            final Title title = Title.of(maintopicCreateRequest.getTitle());
+            final Description description = Description.of(maintopicCreateRequest.getDescription());
+            final Maintopic maintopic = Maintopic.create(title, description);
+            
+            // Save the Maintopic entity
+            final Maintopic savedMaintopic = maintopicRepository.createMaintopic(maintopic);
+            return new MaintopicCreateResponse(
+                savedMaintopic.getMaintopicId(),
+                savedMaintopic.getTitle(),
+                savedMaintopic.getDescription(),
+                savedMaintopic.getCreatedAt().toString()
+            );
+        }catch (Exception e){
+            logger.error("Error creating maintopic: {}", e);
+            throw new InternalServerErrorException("登録に失敗しました。","INTERNAL_SERVER_ERROR");
+        }
         
-        // Save the Maintopic entity
-        final Maintopic savedMaintopic = maintopicRepository.createMaintopic(maintopic);
-        return new MaintopicCreateResponse(
-            savedMaintopic.getMaintopicId(),
-            savedMaintopic.getTitle(),
-            savedMaintopic.getDescription(),
-            savedMaintopic.getCreatedAt().toString()
-        );
     }
 }
