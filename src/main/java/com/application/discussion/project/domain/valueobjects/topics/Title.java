390602f2110e41057f6cb277d17d5e48efa98404
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-08-10 18:02:23 +0900

    feat: (me) #23 update file, Replaced BadRequestException with a custom exception class in the constructor for handling errors.

--- src/main/java/com/application/discussion/project/domain/valueobjects/topics/Title.java
+++ src/main/java/com/application/discussion/project/domain/valueobjects/topics/Title.java
@@ -3,6 +3,8 @@ package com.application.discussion.project.domain.valueobjects.topics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.application.discussion.project.domain.exceptions.BadRequestException;
+
 public class Title {
     private final String value;
     private static final int MAX_LENGTH = 100;
@@ -14,11 +16,11 @@ public class Title {
     private Title(String value) {
         if (value == null || value.isBlank()) {
             logger.error("Title cannot be null or empty: {}", value);
-            throw new IllegalArgumentException("タイトルが空です");
+            throw new BadRequestException("タイトルが空です", "Bad_Request");
         }
         if (value.length() < MIN_LENGTH || value.length() > MAX_LENGTH){
             logger.error("Title must be between {} and {} characters: {}", MIN_LENGTH, MAX_LENGTH, value);
-            throw new IllegalArgumentException("タイトルの文字数は " + MIN_LENGTH + " と " + MAX_LENGTH + " の間でなければなりません");
+            throw new BadRequestException("タイトルの文字数は " + MIN_LENGTH + " と " + MAX_LENGTH + " の間でなければなりません","Bad_Request");
         }
         logger.info("Title value object created with value: {}", value);
         this.value = value;
