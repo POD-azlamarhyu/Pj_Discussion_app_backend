75df9c36b78fd3fed808051cf8f6f318ff7d2eb9
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-08-10 18:28:20 +0900

    feat: (me) #23 add file, Implemented test code for the Description value object.

---
+++ src/test/java/com/application/discussion/project/domain/valueobjects/topics/DescriptionTests.java
@@ -0,0 +1,72 @@
+package com.application.discussion.project.domain.valueobjects.topics;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import com.application.discussion.project.domain.exceptions.BadRequestException;
+
+class DescriptionTests {
+    private Description testDescription;
+    private final String testValue = "これはテストの説明文です。";
+    
+
+    @BeforeEach
+    void setUp() {
+        testDescription = Description.of(testValue);
+    }
+
+    @Test
+    void testValidInstance(){
+        assertNotNull(testDescription);
+        assertInstanceOf(Description.class, testDescription);
+    }
+
+    @Test
+    void testValidDescriptionCreateInstance() {
+        String value = "これは有効な説明文です。";
+        Description description = Description.of(value);
+        assertNotNull(description);
+        assertInstanceOf(Description.class, description);
+        assertEquals(value, description.getValue());
+    }
+
+    @Test
+    void testMaxLengthDescription() {
+        String value = "あ".repeat(500);
+        Description description = Description.of(value);
+        assertNotNull(description);
+        assertInstanceOf(Description.class, description);
+        assertEquals(value, description.getValue());
+    }
+
+    @Test
+    void testMinLengthDescription() {
+        String value = "FizzBazzF";
+        Description description = Description.of(value);
+        assertEquals(value, description.getValue());
+    }
+
+    @Test
+    void nullDescriptionThrowException() {
+        assertThrows(BadRequestException.class, () -> {
+            Description.of(null);
+        });
+    }
+
+    @Test
+    void emptyDescriptionThrowException() {
+        assertThrows(BadRequestException.class, () -> {
+            Description.of("");
+        });
+    }
+
+    @Test
+    void tooLongDescriptionThrowException() {
+        String value = "a".repeat(501);
+        assertThrows(BadRequestException.class, () -> {
+            Description.of(value);
+        });
+    }
+}
