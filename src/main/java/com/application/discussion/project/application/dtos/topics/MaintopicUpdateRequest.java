f0333377a56664d3827c13ad4b7b2c79c0031029
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-08-31 22:32:07 +0900

    feat: (me) #24 Add DTO for request body to represent main topic update details.

---
+++ src/main/java/com/application/discussion/project/application/dtos/topics/MaintopicUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.application.discussion.project.application.dtos.topics;
+
+public class MaintopicUpdateRequest {
+    private String title;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
