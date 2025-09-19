b6b6f81e9c426ed55e15ab22528a6e288dfc0403
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-09-20 04:18:21 +0900

    feat: (me) #26 implement exception class for application layer errors

---
+++ src/main/java/com/application/discussion/project/application/dtos/exceptions/ApplicationLayerException.java
@@ -0,0 +1,51 @@
+package com.application.discussion.project.application.dtos.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+public class ApplicationLayerException extends RuntimeException {
+    private HttpStatusCode code;
+    private HttpStatus status;
+
+    /**
+     * デフォルトコンストラクタ
+     * メッセージ、ステータス、コードなしで例外を作成する
+     */
+    public ApplicationLayerException(){}
+
+    /**
+     * メッセージ、HTTPステータス、HTTPステータスコードを指定するコンストラクタ
+     * @param message エラーメッセージ
+     * @param status HTTPステータス
+     * @param code HTTPステータスコード
+     */
+    public ApplicationLayerException(String message, HttpStatus status,HttpStatusCode code) {
+        super(message);
+        this.status = status;
+        this.code = code;
+    }
+
+    /**
+     * メッセージのみを指定するコンストラクタ
+     * @param message エラーメッセージ
+     */
+    public ApplicationLayerException(String message) {
+        super(message);
+    }
+
+    /**
+     * HTTPステータスコードを取得する
+     * @return HTTPステータスコード
+     */
+    public HttpStatusCode getCode() {
+        return this.code;
+    }
+    
+    /**
+     * HTTPステータスを取得する
+     * @return HTTPステータス
+     */
+    public HttpStatus getStatus() {
+        return this.status;
+    }
+}
