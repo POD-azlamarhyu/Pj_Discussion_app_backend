b82bc63b1479d1fbe9ceee5e98986112ae20fb2a
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-09-20 17:31:46 +0900

    docs: (me) #26 add comment for response message based on GitHub Copilot review

--- src/main/java/com/application/discussion/project/application/dtos/topics/MaintopicDeleteResponse.java
+++ src/main/java/com/application/discussion/project/application/dtos/topics/MaintopicDeleteResponse.java
@@ -5,6 +5,9 @@ public class MaintopicDeleteResponse {
     private String message;
 
     /**
+    * TODO: メッセージの内容は204ステータスに合わせている．Github Copilotのレビューのように削除が成功したことのメッセージに変更することを検討する．
+    */
+    /**
      * デフォルトコンストラクタ
      */
     public MaintopicDeleteResponse(){
