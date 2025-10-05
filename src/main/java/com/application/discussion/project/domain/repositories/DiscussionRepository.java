09ac26f1e8f1cd392a489f606b1970e81a14eacb
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-10-05 22:18:41 +0900

    feat: (me) #19 implement DiscussionRepository interface in domain layer for dependency inversion principle

---
+++ src/main/java/com/application/discussion/project/domain/repositories/DiscussionRepository.java
@@ -0,0 +1,19 @@
+package com.application.discussion.project.domain.repositories;
+
+import com.application.discussion.project.domain.entities.discussions.Discussion;
+
+/**
+ * ディスカッションリポジトリインターフェイス
+ * ディスカッションエンティティの永続化処理を定義するドメイン層のリポジトリ
+ * 実装はインフラストラクチャ層で行われる
+ */
+public interface DiscussionRepository {
+    
+    /**
+     * ディスカッションを新規作成してデータストアに保存する
+     * 
+     * @param discussion 保存するディスカッションエンティティ
+     * @return 保存されたディスカッションエンティティ（IDや日時が設定済み）
+     */
+    Discussion createDiscussion(Discussion discussion);
+}
