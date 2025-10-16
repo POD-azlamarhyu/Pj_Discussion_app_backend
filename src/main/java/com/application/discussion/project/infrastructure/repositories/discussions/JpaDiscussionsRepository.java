5489574789b66b2e061d2e9709ed477244dd096c
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-10-17 00:38:52 +0900

    feat: (me) #20 implement JPA repository method to retrieve discussion content

--- src/main/java/com/application/discussion/project/infrastructure/repositories/discussions/JpaDiscussionsRepository.java
+++ src/main/java/com/application/discussion/project/infrastructure/repositories/discussions/JpaDiscussionsRepository.java
@@ -1,9 +1,12 @@
 package com.application.discussion.project.infrastructure.repositories.discussions;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.application.discussion.project.infrastructure.models.discussions.Discussions;
 
 public interface JpaDiscussionsRepository extends JpaRepository<Discussions, Long> {
-
+    Discussions findByDiscussionId(Long discussionId);
+    List<Discussions> findByMaintopicId(Long maintopicId);
 }
