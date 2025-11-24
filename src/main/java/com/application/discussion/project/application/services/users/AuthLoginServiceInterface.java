48776821aee807cd331fd936563406204829a48b
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-11-24 09:13:58 +0900

    feat: (me) #9 Implement login application service interface and apply type-safety improvements

--- src/main/java/com/application/discussion/project/application/services/users/AuthLoginServiceInterface.java
+++ src/main/java/com/application/discussion/project/application/services/users/AuthLoginServiceInterface.java
@@ -3,9 +3,10 @@ package com.application.discussion.project.application.services.users;
 import org.springframework.http.ResponseEntity;
 
 import com.application.discussion.project.application.dtos.users.LoginRequest;
+import com.application.discussion.project.application.dtos.users.LoginResponse;
 
 public interface AuthLoginServiceInterface {
-    ResponseEntity<?> service(
-        LoginRequest loginRequest
+    ResponseEntity<LoginResponse> service(
+        final LoginRequest loginRequest
     );
 }
