c9d02dc542d95d6908b4b9a67ab73675ba00f0c1
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-11-24 09:29:18 +0900

    refactor: (me) #9 Replace generic exceptions with domain-specific exceptions and mark method parameters final to enforce immutability

--- src/main/java/com/application/discussion/project/domain/valueobjects/users/UserName.java
+++ src/main/java/com/application/discussion/project/domain/valueobjects/users/UserName.java
@@ -1,23 +1,28 @@
 package com.application.discussion.project.domain.valueobjects.users;
 
+import com.application.discussion.project.domain.exceptions.DomainLayerErrorException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
 public class UserName {
     private final String userName;
     private final static Integer maxLength = 255;
     private final static Integer minLength = 1;
 
-    private UserName(String userName) {
+    private UserName(final String userName) {
         this.validate(userName);
         this.userName = userName;
     }
-    private void validate(String userName) {
+    private void validate(final String userName) {
         if (userName == null || userName.isEmpty()) {
-            throw new IllegalArgumentException("User name cannot be null or empty");
+            throw new DomainLayerErrorException("ユーザー名は必須項目です",HttpStatus.BAD_REQUEST , HttpStatusCode.valueOf(400));
         }
         if (userName.length() > maxLength || userName.length() < minLength) {
-            throw new IllegalArgumentException("User name must be between " + minLength + " and " + maxLength + " characters");
+            throw new DomainLayerErrorException("ユーザー名は" + minLength + "文字以上" + maxLength + "文字以下である必要があります",HttpStatus.BAD_REQUEST , HttpStatusCode.valueOf(400));
         }
     }
-    public static UserName of(String userName) {
+    public static UserName of(final String userName) {
         return new UserName(userName);
     }
     public String value() {
