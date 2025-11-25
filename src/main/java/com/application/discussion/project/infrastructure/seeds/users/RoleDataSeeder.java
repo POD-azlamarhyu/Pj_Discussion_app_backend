2a275f89f8324b795af6f0980095504ec695fcaa
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-11-25 23:06:53 +0900

    chore: (me) #9 Add annotations to prevent execution in production environment in response to Copilot review

--- src/main/java/com/application/discussion/project/infrastructure/seeds/users/RoleDataSeeder.java
+++ src/main/java/com/application/discussion/project/infrastructure/seeds/users/RoleDataSeeder.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.application.discussion.project.infrastructure.models.users.Roles;
 import com.application.discussion.project.infrastructure.repositories.users.JpaRolesRepository;
 
 @Component
+@Profile("dev")
 public class RoleDataSeeder {
 
     private final List<String> roles = List.of(
