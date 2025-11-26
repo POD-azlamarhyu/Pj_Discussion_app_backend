6103da45f25a2de56ced08c5c7fef660e8fbd31a
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-11-27 01:04:34 +0900

    chore: (me) Removed unused imports

--- src/main/java/com/application/discussion/project/infrastructure/repositories/users/JpaUsersRepository.java
+++ src/main/java/com/application/discussion/project/infrastructure/repositories/users/JpaUsersRepository.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.application.discussion.project.infrastructure.models.users.Users;
