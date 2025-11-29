ab05c78542caded4e1015bad9bbb44e2480cd05c
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-11-29 18:55:48 +0900

    chore: (me) Removed unused imports

--- src/test/java/com/application/discussion/project/presentation/controllers/RestAPIControllerTest.java
+++ src/test/java/com/application/discussion/project/presentation/controllers/RestAPIControllerTest.java
@@ -8,14 +8,12 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.application.discussion.project.presentation.config.WebSecurityConfig;
 import com.application.discussion.project.presentation.exceptions.GlobalExceptionHandler;
 
 @ContextConfiguration
-// @ActiveProfiles("dev")
 @SpringBootTest
 @AutoConfigureMockMvc
 @Target(ElementType.TYPE)
