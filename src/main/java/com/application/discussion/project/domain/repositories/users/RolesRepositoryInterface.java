7aad701583ad3238ba91009a5a09043cd0e3f9ac
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-12-16 03:44:31 +0900

    refactor: (me) #89 Refactor repository interface implementation

--- src/main/java/com/application/discussion/project/domain/repositories/users/RolesRepositoryInterface.java
+++ src/main/java/com/application/discussion/project/domain/repositories/users/RolesRepositoryInterface.java
@@ -3,10 +3,10 @@ package com.application.discussion.project.domain.repositories.users;
 import java.util.Set;
 import java.util.UUID;
 
-
+import com.application.discussion.project.domain.entities.users.Role;
 import com.application.discussion.project.infrastructure.models.users.Roles;
 
 public interface RolesRepositoryInterface {
 
-    Set<Roles> findUserRolesById(UUID userId);
+    Set<Role> findUserRolesById(UUID userId);
 }
