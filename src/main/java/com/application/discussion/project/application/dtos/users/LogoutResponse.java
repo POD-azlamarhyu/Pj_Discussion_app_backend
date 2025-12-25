71bb9aaad49317dcc4c4192318b41b34302117af
Jiro-freedom <sgommccisbad72145@gmail.com>
2025-12-25 22:57:37 +0900

    refactor: (me) #10 Remove Lombok from LogoutResponseDTO and implement manual constructors and getters for better logging integration

--- src/main/java/com/application/discussion/project/application/dtos/users/LogoutResponse.java
+++ src/main/java/com/application/discussion/project/application/dtos/users/LogoutResponse.java
@@ -1,17 +1,8 @@
 package com.application.discussion.project.application.dtos.users;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * ログアウトレスポンスDTO
  */
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class LogoutResponse {
     
     /**
@@ -23,4 +14,46 @@ public class LogoutResponse {
      * 成功フラグ
      */
     private Boolean success;
+
+    /**
+     * デフォルトコンストラクタ
+     */
+    public LogoutResponse() {
+    }
+
+    /**
+     * 全フィールドを初期化するコンストラクタ
+     * 
+     * @param message メッセージ
+     * @param success 成功フラグ
+     */
+    private LogoutResponse(String message, Boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public static LogoutResponse of(String message, Boolean success) {
+        return new LogoutResponse(message, success);
+    }
+
+    /**
+     * メッセージを取得する
+     * @return メッセージ
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * 成功フラグを取得する
+     * @return 成功フラグ
+     */
+    public Boolean getSuccess() {
+        return success;
+    }
+    
+    @Override
+    public String toString() {
+        return "LogoutResponse{message='" + message + "', success=" + success + "}";
+    }
 }
