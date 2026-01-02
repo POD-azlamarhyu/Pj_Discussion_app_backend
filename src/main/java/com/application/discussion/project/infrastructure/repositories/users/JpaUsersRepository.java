bc8fc42b550697190b4bf5b994f79a55c8077fd1
Jiro-freedom <sgommccisbad72145@gmail.com>
2026-01-03 03:21:09 +0900

    refactor: (me) Remove unused SQL queries and reduce redundant code

--- src/main/java/com/application/discussion/project/infrastructure/repositories/users/JpaUsersRepository.java
+++ src/main/java/com/application/discussion/project/infrastructure/repositories/users/JpaUsersRepository.java
@@ -4,20 +4,14 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.application.discussion.project.infrastructure.models.users.Users;
 
 @Repository
 public interface JpaUsersRepository extends JpaRepository<Users, UUID> {
-
-    @Query(
-        value="select * from users as u where u.login_id = ?1 or u.email = ?2",
-        nativeQuery=true
-    )
-    Optional<Users> findByEmailOrLoginId(String email,String loginId);
-
+    
+    Optional<Users> findByEmailOrLoginId(String email, String loginId);
+    
     Boolean existsByEmail(String email);
-    Boolean existsByLoginId(String loginId);
 }
