d1c5d3f2df6fc92d542edaed64558c84d5a94901
Jiro-freedom <sgommccisbad72145@gmail.com>
2026-01-09 13:57:00 +0900

    feat: (me) #101 Create custom exception class to handle exceptions in the infrastructure layer

---
+++ src/main/java/com/application/discussion/project/infrastructure/exceptions/InfrastructureLayerErrorException.java
@@ -0,0 +1,37 @@
+package com.application.discussion.project.infrastructure.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+public class InfrastructureLayerErrorException extends RuntimeException {
+
+    private final HttpStatusCode code;
+    private final HttpStatus status;
+
+    /**
+     * デフォルトコンストラクタ
+     * メッセージ、ステータス、コードなしで例外を作成する
+     */
+    public InfrastructureLayerErrorException(
+        String message,HttpStatus status, HttpStatusCode code
+    ) {
+        super(message);
+        this.status = status;
+        this.code = code;
+    }
+
+    /**
+     * HTTPステータスコードを取得する
+     * @return
+     */
+    public HttpStatusCode getCode() {
+        return this.code;
+    }
+    /**
+     * HTTPステータスを取得する
+     * @return HTTPステータス
+     */
+    public HttpStatus getStatus() {
+        return this.status;
+    }
+}
